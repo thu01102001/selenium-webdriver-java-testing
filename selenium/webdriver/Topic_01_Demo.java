0e83f70311f9371f47bb7c2b326351171c283c39
thu thu <nguyenthilethu2001vn@gmail.com>
2024-03-08 16:31:50 +0700

    add new class

--- selenium/webdriver/Topic_01_Demo.java
+++ selenium/webdriver/Topic_01_Demo.java
@@ -2,6 +2,6 @@ package webdriver;
 
 public class Topic_01_Demo {
     public static void main(String[] args) {
-        System.out.printf("Hello world");
+        System.out.printf("Hello world  ");
     }
 }
