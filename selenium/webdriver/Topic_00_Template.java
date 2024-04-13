edfca61fd2827b2f107c2ba07ccb7e651bd05f35
thu thu <nguyenthilethu2001vn@gmail.com>
2024-04-13 17:25:13 +0700

    update check css color hex

--- selenium/webdriver/Topic_00_Template.java
+++ selenium/webdriver/Topic_00_Template.java
@@ -1,8 +1,11 @@
 package webdriver;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.Color;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -12,14 +15,22 @@ public class Topic_00_Template {
     WebDriver driver; //khai báo bến
     @BeforeClass
     public void InitialBrowser() {
-        driver = new ChromeDriver();
+        driver = new FirefoxDriver();
         driver.get("https://demo.nopcommerce.com");
     }
 //
-    //2. action / execute: tương tác lên element nào / nhập liệu / verify/
     @Test
     public void TC_01_() {
-
+        WebElement element = driver.findElement(By.xpath("//button[@type='submit']"));
+        String color = element.getCssValue("font-size");
+        Assert.assertEquals(color, "14px");
+        //hex
+        String color1 = element.getCssValue("color");
+        String color2 = Color.fromString(color1).asHex();
+        Assert.assertEquals(color2, "#fff");
+        WebElement placeholder = driver.findElement(By.xpath("//input[@id='small-searchterms']"));
+        String placeholder1 = placeholder.getAttribute("placeholder");
+        Assert.assertEquals(placeholder1, "Search store");
     }
     @Test
     public void TC_02_() {
